a368171c0c2c43c580fe8f8543dd19802c289205
ben7th <ben7th@sina.com>
2010-09-08 11:27:43 +0800

    db

--- db/schema.rb
+++ db/schema.rb
@@ -11,37 +11,6 @@
 
 ActiveRecord::Schema.define(:version => 20100816010748) do
 
-  create_table "achievements", :force => true do |t|
-    t.integer  "user_id"
-    t.string   "honor"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "achievements", ["honor"], :name => "index_achievementings_on_achievement_id"
-  add_index "achievements", ["user_id"], :name => "index_achievementings_on_user_id"
-
-  create_table "contactings", :force => true do |t|
-    t.integer  "host_id"
-    t.integer  "contact_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "show_share"
-  end
-
-  add_index "contactings", ["contact_id"], :name => "index_contacts_on_friend_id"
-  add_index "contactings", ["host_id"], :name => "index_contacts_on_host_id"
-
-  create_table "invitations", :force => true do |t|
-    t.integer  "host_id"
-    t.string   "contact_email"
-    t.string   "uuid"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "invitations", ["host_id"], :name => "index_invitations_on_host_id"
-
   create_table "online_records", :force => true do |t|
     t.integer  "user_id"
     t.string   "key"
@@ -62,18 +31,6 @@ ActiveRecord::Schema.define(:version => 20100816010748) do
   end
 
   add_index "preferences", ["user_id"], :name => "index_preferences_on_user_id"
-
-  create_table "system_settings", :force => true do |t|
-    t.string   "name"
-    t.text     "footer_info"
-    t.text     "login_info"
-    t.string   "logo_file_name"
-    t.string   "logo_content_type"
-    t.integer  "logo_file_size"
-    t.datetime "logo_updated_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "users", :force => true do |t|
     t.string   "name",                      :default => "", :null => false
