e9419c07100e8ca433f7fb8ddc1f95a7a691731e
ben7th <ben7th@sina.com>
2010-11-01 15:44:58 +0800

    user cookie

--- lib/sessions_methods.rb
+++ lib/sessions_methods.rb
@@ -32,10 +32,11 @@ module SessionsMethods
   def reset_session_with_online_key
     online_key=session[:online_key]
     reset_session
-    session[:online_key]=online_key
-    flash.now[:notice]="您已经退出了"
 
     delete_logged_in_for_plugin_token # 清除插件cookie
+
+    session[:online_key]=online_key
+    flash.now[:notice]="您已经退出了"
   end
 
   def destroy_cookie_token
